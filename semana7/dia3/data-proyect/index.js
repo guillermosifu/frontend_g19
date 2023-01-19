30c8890a30aef6c31b89d3735deedea60c5ba150
guillermosifu <sifuentes1201@gmail.com>
2023-01-18 19:39:16 -0500

    memes

---
+++ semana7/dia3/data-proyect/index.js
@@ -0,0 +1,24 @@
+const listaInterna =()=>{
+    const meme =[
+        {
+            nombre:"me lo juras",
+            imagen:"https://i.ytimg.com/vi/PmiERz5tqEU/maxresdefault.jpg",
+            enlace: "https://c.tenor.com/werGAdQw-8sAAAAd/neta-me-lo-juras-nodding.gif"
+
+        },
+        {
+            nombre:"no lo se rick",
+            imagen:"https://i.postimg.cc/qqcN5sz4/No-lo-se-Rick.jpg",
+            enlace: "https://c.tenor.com/werGAdQw-8sAAAAd/neta-me-lo-juras-nodding.gif"
+
+        },
+        {
+            nombre:"me estas retando",
+            imagen:"https://i.postimg.cc/yNSYGmbt/3497e5b81c50a22de4d840cfb151dfb5.jpg",
+            enlace: "https://c.tenor.com/werGAdQw-8sAAAAd/neta-me-lo-juras-nodding.gif"
+
+        }
+    ]
+
+}
+
